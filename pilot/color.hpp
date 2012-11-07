33bca1d3b62053474fb8e455c211f93f4361ea7c
shinarit <tetra666@gmail.com>
2012-11-07 17:30:28 +0100

    to squash

--- pilot/color.hpp
+++ pilot/color.hpp
@@ -6,7 +6,7 @@ namespace Color
   const int RED_MASK    = 0xff000000;
   const int BLUE_MASK   = 0x00ff0000;
   const int GREEN_MASK  = 0x0000ff00;
-  
+
   //int should be at least 32 bits
   typedef int Pixel;
   //r, g and b should be [0, 255]
@@ -14,7 +14,7 @@ namespace Color
   {
     return (red << 16) + (green << 8) + blue;
   }
-  
+
   const Pixel BLACK = 0;
 }
 
--- pilot/physics.hpp
+++ pilot/physics.hpp
@@ -12,18 +12,22 @@ class Physics
     static const float Height;
 
     //creates a box from 0, 0 (topleft corner) to width - 1, height - 1 (botright corner), with gravity down
-    Physics();
+    Physics(float width, float height);
     ~Physics();
 
     b2Body* addBox(Vector center, int width, int height);
     void print(b2Body* body);
     void step();
-    
+    float getWidth();
+    float getHeight();
+
   private:
     static const int velocityIterations = 8;
     static const int positionIterations = 2;
     static const float timeStep;
 
+    const float m_width;
+    const float m_height;
     b2World m_world;
 };
 
