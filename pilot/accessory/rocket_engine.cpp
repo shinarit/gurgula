6b37eacee7f45603a8d8207e5af7e6c0c2d3a342
tetra <tetra666@gmail.com>
2013-01-26 12:19:45 +0100

    adding basic rocket engine

---
+++ pilot/accessory/rocket_engine.cpp
@@ -0,0 +1,5 @@
+#include "rocket_engine.hpp"
+
+RocketEngine::RocketEngine(ACCESSORY_PARAMETERS): INITIALIZE_ACCESSORY_BASE()
+{
+}
---
+++ pilot/accessory/rocket_engine.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "accessory.hpp"
+
+class RocketEngine: public Accessory
+{
+  public:
+    RocketEngine(ACCESSORY_PARAMETERS);
+};
