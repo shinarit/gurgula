672a808797123a1180cc2667b2f041a0ee30e048
tetra <tetra666@gmail.com>
2013-02-10 11:18:46 +0100

    adding some documentation

--- pilot/basic_elements.hpp
+++ pilot/basic_elements.hpp
@@ -4,15 +4,12 @@
 
 #include <Box2D/Common/b2Math.h>
 
+/// general measurement typedefs
 typedef double Direction;
 typedef double Length;
 
+/// vector functionality
 typedef b2Vec2 Vector;
-
-//should be "uncomplete", meaning the last and the first vertex shouldnt be the same
-typedef std::vector<Vector> Polygon;
-typedef std::vector<Polygon> PolygonList;
-
 //angle is in radians
 Vector rotate(const Vector& vector, float angle);
 //translates vector from x1 x y1 to x2 x y2
@@ -20,3 +17,8 @@ Vector distort(const Vector& vector, int x1, int y1, int x2, int y2);
 
 //from polar
 Vector fromPolar(Direction direction, Length length);
+
+/// shape descriptors
+//should be "uncomplete", meaning the last and the first vertex shouldnt be the same
+typedef std::vector<Vector> Polygon;
+typedef std::vector<Polygon> PolygonList;
